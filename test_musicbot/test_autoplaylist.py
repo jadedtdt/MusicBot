b3bb3b81248de6eb68e91f70fb8ad2ee979952c3
jadedtdt <genebbb333@aol.com>
2019-05-26 14:54:42 -0400

    updating merge conflict

--- test_musicbot/test_autoplaylist.py
+++ test_musicbot/test_autoplaylist.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from musicbot.autoplaylist import AutoPlaylist
 from musicbot.sqlfactory import SqlFactory
 
@@ -13,20 +12,6 @@ def test_default_songs(vanilla_apl):
 
 def test_default_users(vanilla_apl):
     assert str(vanilla_apl.users) == str(vanilla_apl._get_users())
-=======
-from autoplaylist import Autoplaylist
-from sqlfactory import SqlFactory
-
-@pytest.fixture
-def vanilla_apl():
-    return Autoplaylist()
-
-def test_default_songs(vanilla_apl):
-    assert vanilla_apl.songs == vanilla_apl._get_songs()
-
-def test_default_users(vanilla_apl):
-    assert vanilla_apl.users == vanilla_apl._get_users()
->>>>>>> 32a94346d86f1aa74f49a3cb13a577b0308f6956
 
 def test_default_sqlfactory(vanilla_apl):
     assert isinstance(vanilla_apl.sqlfactory, SqlFactory)
